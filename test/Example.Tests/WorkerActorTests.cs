754286d02e0518ee482970006f8d7e2784ff018d
Bruno Alencar <bruno.idealencar@gmail.com>
2018-03-24 13:44:15 -0300

    Add test kit for actors

---
+++ test/Example.Tests/WorkerActorTests.cs
@@ -0,0 +1,19 @@
+using Akka.TestKit.Xunit2;
+using Xunit;
+
+namespace Example.Tests
+{
+    public class WorkerActorTests : TestKit
+    {
+        public WorkerActorTests() 
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
