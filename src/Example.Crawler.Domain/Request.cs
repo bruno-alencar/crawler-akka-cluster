8908da76d77cfb380f97dff15c7b74b3be1987e0
Bruno Alencar <bruno.idealencar@gmail.com>
2018-04-08 23:16:06 -0300

    Add request file on project

---
+++ src/Example.Crawler.Domain/Request.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Example.Crawler.Domain
+{
+    public class Request
+    {
+        private HttpClient _httpClient;
+
+        public Request(HttpClient httpClient = null)
+        {
+            _httpClient = httpClient ?? new HttpClient();
+        }
+
+        public string PreRequest(string uri = null)
+        {
+            if (string.IsNullOrWhiteSpace(uri))
+                return "error";
+
+            _httpClient.BaseAddress = new Uri(uri);
+            //_httpClient.DefaultRequestHeaders;
+
+            return null;
+        }
+
+        public void Scrap()
+        {
+
+        }
+
+        public void PostScrap()
+        {
+
+        }
+    }
+}
