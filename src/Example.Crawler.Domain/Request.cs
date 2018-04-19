c75e9c93bd7810dc24bf29d4e904a7846c6b7765
Bruno Alencar <bruno.idealencar@gmail.com>
2018-04-19 09:09:24 -0300

    Remove error message

--- src/Example.Crawler.Domain/Request.cs
+++ src/Example.Crawler.Domain/Request.cs
@@ -11,13 +11,6 @@ namespace Example.Crawler.Domain
 {
     public class Request
     {
-
-//        <!--
-//Need permission to access data? Contact: DataAccess @datadome.co
-//-->
-//<html><head><title>You have been blocked</title></head><body style = "margin:0" >< script > var dd={ 'cid':'AHrlqAAAAAMAhbp-LxIg9ywAs79lIg==','hsh':'C499C5254821BA7F386B459241B3FC'}</script><script src = "https://ct.datadome.co/c.js" ></ script >< noscript > Please enable JavaScript</noscript></body></html>
-
-
         private HttpClient _httpClient;
         private HttpWebRequest _request;
         private const string DefaultUrl = "http://www.tudogostoso.com.br/";
@@ -64,7 +57,6 @@ namespace Example.Crawler.Domain
                 using (var client = new HttpClient(handler) { BaseAddress = baseAddress })
                 {
                     var message = new HttpRequestMessage(HttpMethod.Get, "/");
-                    //message.Headers.Add("Cookie", "datadome=AHrlqAAAAAMAhbp-LxIg9ywAs79lIg==; nav22518=7bdaa1d61ce4a98495977500609|2_234; _ga=GA1.3.1193982996.1503320096; tt.u=840A000ADE476E59D65E8E69029C9846; trc_cookie_storage=taboola%2520global%253Auser-id%3D2f9a2e0d-b2c6-47cc-9620-6edd8f7ec8d7-tuct67cd4d; _gid=GA1.3.358097844.1523842463; __gads=ID=432bb6e3ee1f0abb:T=1523842446:S=ALNI_MYcVKv4fb23AwCkDNYKD_W1Zd-YfA; tt.nprf=; tt_c_vmt=1523896790; tt_c_c=direct; tt_c_s=direct; tt_c_m=direct; _ttuu.s=1523896790706 If-None-Match:W/\"907f0058e317e22e9eb65ae7269f10b0\"");
                     message.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1");
                     var result = await client.SendAsync(message);
                     return await result.Content.ReadAsStringAsync();
