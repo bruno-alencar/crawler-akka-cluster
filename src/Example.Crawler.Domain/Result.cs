d448a8186c51c8ebc1c308fa5e791b3be6df62a5
Bruno Alencar <bruno.idealencar@gmail.com>
2018-04-20 10:32:17 -0300

    Start to create result class

---
+++ src/Example.Crawler.Domain/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Crawler.Domain
+{
+    public enum ResultStatusCode
+    {
+        OK,
+        Error,
+        NotFound,
+        BadRequest
+    }
+    public class Result<T>
+    {
+        public ResultStatusCode Status { get; set; }
+        public object Value { get; set; }
+        public object ValueAsSuccess { get; set; }
+
+        public Result(ResultStatusCode status, object value)
+        {
+            Status = status;
+            Value = value;
+            ValueAsSuccess = (T)value;
+        }
+    }
+}
