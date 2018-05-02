48ebacf6e28459944122b6bc99915f8bc8c5c935
Bruno Alencar <bruno.idealencar@gmail.com>
2018-05-01 23:16:37 -0300

    Add uri on message domain class

--- src/Example.Crawler.Domain/Common/Messages.cs
+++ src/Example.Crawler.Domain/Common/Messages.cs
@@ -6,7 +6,10 @@ namespace Example.Crawler.Domain.Common
 {
     public class Configure { }
 
-    public class PreScrap { }
+    public class PreScrap
+    {
+        public string Uri { get; set; }
+    }
 
     public class Scrap { }
 
